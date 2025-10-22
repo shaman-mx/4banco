1ae3b34edd121962aeb0db35726e2682aea38a89
shaman-mx <shamanmx@gmail.com>
2025-10-23 06:37:35 +0700

    Delete cpp_ai directory

--- cpp_ai/util/create_param_arr.py
+++
@@ -1,9 +0,0 @@
-with open('param.txt', 'r') as f:
-  data = f.read().splitlines()
-
-with open('param_cpp.cpp', 'w') as f:
-  f.write('{')
-  for elem in data[:-1]:
-      f.write(elem + ',')
-  f.write(data[-1])
-  f.write('};\n')
